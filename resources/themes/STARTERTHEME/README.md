94ddae3c3ae8586c0a4d84a247210c88c5ce7024
Camilla Krag Jensen <camilla@reload.dk>
2014-02-28 14:33:16 +0100

    Docs and cleanup in theme.

--- resources/themes/STARTERTHEME/README.md
+++ resources/themes/STARTERTHEME/README.md
@@ -1,5 +1,12 @@
 # STARTERTHEME theme
 
+# TODO
+This theme was started from the starter theme in Reload\!s start-platform. To start using this theme do these steps:
+
+ * Replace logo.png with ont that makes sense for your theme.
+ * Replace the favicon - unless you are Simon.
+ * Replace the screenshot.png file.
+
 ## Set up
 If you don't have Grunt installed, then go follow the [Getting started](http://gruntjs.com/getting-started) guide.
 
--- resources/themes/STARTERTHEME/template.php
+++ resources/themes/STARTERTHEME/template.php
@@ -1,5 +1,28 @@
 <?php
 /**
  * @file
- * Main template file.
+ * Theming functions for STARTERTHEME.
  */
+
+/**
+ * Override theme_menu_link().
+ */
+function STARTERTHEME_menu_link(array $variables) {
+  $element = $variables['element'];
+  $sub_menu = '';
+
+  if ($element['#below']) {
+    $sub_menu = drupal_render($element['#below']);
+  }
+
+  $output = l($element['#title'], $element['#href'], $element['#localized_options']);
+
+  // If the link is a nolink one (see hook_menu() in reload_f5.module), then
+  // do some extra stuff and overwrite $output.
+  if (preg_match('{<nolink>}', $element['#href'])) {
+    $output = '<span>' . strip_tags($output) . '</span>';
+    $element['#attributes']['class'][] = 'nolink';
+  }
+
+  return '<li' . drupal_attributes($element['#attributes']) . '>' . $output . $sub_menu . "</li>\n";
+}
